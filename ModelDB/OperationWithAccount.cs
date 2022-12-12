ce71370eff0d1071eb873aeac1b762c1be7c690a
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-12-12 21:31:47 +0200

    Added classes for work with DB

---
+++ ModelDB/OperationWithAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public class OperationWithAccount 
+    {
+        public async Task RegistrationAsync(string? name, string? surname, string? email, string? password)
+        {
+            Accounts account = new Accounts(name,surname,email, password);
+            await SaveLoadfromDB.AddAccountDBAsync(account);
+        }
+
+        public async Task<Accounts> SignInAsync(string email)
+        {
+            return await SaveLoadfromDB.GetAccountAsync(email);
+        }
+    }
+}
---
+++ ModelDB/SaveLoadfromDB.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Crypto
+{
+    public static class SaveLoadfromDB
+    {
+
+        //метод щоб додати акаунт в БД
+        public static async Task AddAccountDBAsync(Accounts accounts)
+        {
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    currencyDB.Accounts.Add(accounts);
+                    await currencyDB.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        //метод, щоб додати дії з покупки валюти до БД
+        //викликати метод - SaveLoadfromDB.AddHistoryDB(history).Wait();
+        public static async Task AddHistoryDBAsync(HistoryAccount history)
+        {
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    currencyDB.HistoryAccount.Add(history);
+                    await currencyDB.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        //метод, щоб додати інформацію про гаманець користувача в БД
+        //викликати метод - SaveLoadfromDB.AddWalletDB(wallet).Wait();
+        public static async Task AddWalletDBAsync(Notecase wallet)
+        {
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    currencyDB.Notecase.Add(wallet);
+                    await currencyDB.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        //метод для отримання операцій конкретного аккаунту за Id
+        //Отримати результат- List<History> history = new List<History>();
+        //var tmp = SaveLoadfromDB.GetAccountHistory(1);
+        //history = tmp.Result;
+        public static async Task<List<HistoryAccount>> GetAccountHistoryAsync(int IdUser)
+        {
+            List<HistoryAccount> history = new List<HistoryAccount>();
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    history = await (from lst in currencyDB.HistoryAccount
+                                     where lst.Id_account == IdUser
+                                     orderby lst.Transaction_date
+                                     select lst).ToListAsync<HistoryAccount>();
+                }
+                if (history.Count == 0)
+                {
+                    throw new Exception("Record not found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return history;
+        }
+        //метод для перевірки наявності акаунту в БД за ел.адресою
+        public static async Task<Accounts> GetAccountAsync(string EmailUser)
+        {
+            Accounts account = new Accounts();
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    account = await (from lst in currencyDB.Accounts
+                                     where lst.Email == EmailUser
+                                     select lst).FirstOrDefaultAsync<Accounts>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return account;
+        }
+
+        //метод для пошуку гаманця користувача в бд
+        public static async Task<Notecase> GetAccountWalletAsync(int IdUser)
+        {
+            Notecase wallet = new Notecase();
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    wallet = await (from lst in currencyDB.Notecase
+                                    where lst.Id_account == IdUser
+                                    select lst).FirstOrDefaultAsync<Notecase>();
+                }
+                if (wallet == null)
+                {
+
+                    throw new Exception("Record not found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return wallet;
+        }
+
+        //метод для зміни суми гаманця в БД
+        public static async Task UpdateWalletAsync(int id, decimal totalSum)
+        {
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    var tmp = currencyDB.Notecase.Where(s => s.Id == id).FirstOrDefault();
+
+                    if (tmp != null)
+                    {
+                        tmp.Sum= totalSum;
+                        currencyDB.Entry(tmp).State= EntityState.Modified;
+                        await currencyDB.SaveChangesAsync();
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        //метод для зміни паролю для акаунту
+        public static async Task UpdatePasswordAsync(string emailUser, string newPassword)
+        {
+            try
+            {
+                using (CryptoCurrencyDB currencyDB = new CryptoCurrencyDB())
+                {
+                    var tmp = currencyDB.Accounts.Where(s => s.Email == emailUser).FirstOrDefault();
+
+                    if (tmp != null)
+                    {
+                        tmp.Password = newPassword;
+                        currencyDB.Entry(tmp).State = EntityState.Modified;
+                        await currencyDB.SaveChangesAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
