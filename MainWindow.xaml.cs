60c3e7119ae54c3e898548d4f8df6cabbae55d64
Evan_xr62 <nane33553@gmail.com>
2022-12-14 16:49:01 +0200

    Search

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,44 @@ namespace Crypto
         public MainWindow()
         {
             InitializeComponent();
+
+            Coins coin = new Coins();
+
+            coin.coinId = "";
+            coin.coinIcon = "";
+            coin.coinNameC = "";
+            coin.coinPriceC = "";
+
+            GridCoins.Items.Add(coin);
+
+            Coins coin1 = new Coins();
+
+            coin1.coinId = "";
+            coin1.coinIcon = "";
+            coin1.coinNameC = "";
+            coin1.coinPriceC = "";
+
+            GridCoins.Items.Add(coin1);
+
+            TradeHistory trHis = new TradeHistory();
+
+            trHis.trHisName = "";
+            trHis.trHisType = "";
+            trHis.trHisPrice = "";
+            trHis.trHisAmount = "";
+            trHis.trHisDoll = "";
+
+            GridTradeHistory.Items.Add(trHis);
+
+            TradeHistory trHis1 = new TradeHistory();
+
+            trHis1.trHisName = "";
+            trHis1.trHisType = "";
+            trHis1.trHisPrice = "";
+            trHis1.trHisAmount = "";
+            trHis1.trHisDoll = "";
+
+            GridTradeHistory.Items.Add(trHis1);
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -36,6 +74,23 @@ namespace Crypto
             win.Content = new Page();
             win.Show();
             this.Close();
+        }
+
+        public class Coins
+        {
+            public string coinId { get; set; }
+            public string coinIcon { get; set; }
+            public string coinNameC { get; set; }
+            public string coinPriceC { get; set; }
+        }
+
+        public class TradeHistory
+        {
+            public string trHisName { get; set; }
+            public string trHisType { get; set; }
+            public string trHisPrice { get; set; }
+            public string trHisAmount { get; set; }
+            public string trHisDoll { get; set; }
         }
     }
 }
--- Page.xaml.cs
+++ Page.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Crypto.MainWindow;
+using static Crypto.Page;
 
 namespace Crypto
 {
@@ -23,6 +25,43 @@ namespace Crypto
         public Page()
         {
             InitializeComponent();
+
+            History hist = new History();
+
+            hist.histData = "";
+            hist.histName = "";
+            hist.histType = "";
+            hist.histPrice = "";
+            hist.histAmount = "";
+            hist.histProfit = "";
+
+            GridHistory.Items.Add(hist);
+
+            History hist1 = new History();
+
+            hist1.histData = "";
+            hist1.histName = "";
+            hist1.histType = "";
+            hist1.histPrice = "";
+            hist1.histAmount = "";
+            hist1.histProfit = "";
+
+            GridHistory.Items.Add(hist1);
+        }
+
+        private void Back_Click(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        public class History
+        {
+            public string histData { get; set; }
+            public string histName { get; set; }
+            public string histType { get; set; }
+            public string histPrice { get; set; }
+            public string histAmount { get; set; }
+            public string histProfit { get; set; }
         }
     }
 }
