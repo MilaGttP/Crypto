48a7ff0c6c4fa053decb763362c41ea5eaf585d8
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-12-14 22:46:11 +0200

    Added class OperationWithNotecase

--- ModelDB/OperationWithAccount.cs
+++ ModelDB/OperationWithAccount.cs
@@ -10,13 +10,40 @@ namespace Crypto
     {
         public void Registration(string? name, string? surname, string? email, string? password)
         {
-            Accounts account = new Accounts(name, surname, email, password);
-            SaveLoadfromDB.AddAccountDB(account);
+            try
+            {
+                Accounts account = new Accounts(name, surname, email, password);
+                SaveLoadfromDB.AddAccountDB(account);
+                var tmp = SaveLoadfromDB.GetAccount(email);
+                Notecase notecase = new Notecase(tmp.Id, email, 0);
+                SaveLoadfromDB.AddWalletDB(notecase);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public Accounts SignIn(string email, ref Notecase notecase)
+        {
+            Accounts accounts = null;
+            try
+            {
+                accounts = SaveLoadfromDB.GetAccount(email);
+                Notecase notecase1 = null;
+                notecase1 = SaveLoadfromDB.GetAccountWallet(accounts.Id);
+                notecase = notecase1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return accounts;
         }
 
-        public Accounts SignIn(string email)
+        public bool IsPasswordCorrect(Accounts accounts, string? password)
         {
-            return SaveLoadfromDB.GetAccount(email);
+            return (accounts.Password == password) ? true : false;
         }
     }
 }
