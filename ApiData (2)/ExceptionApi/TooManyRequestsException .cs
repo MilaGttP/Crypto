86c2a5e1f7513ab197619c96cc1494292090e8c4
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-12-15 19:08:56 +0200

    Replace api data

---
+++ ApiData (2)/ExceptionApi/TooManyRequestsException .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public class TooManyRequestsException : CoinApiException
+    {
+        public TooManyRequestsException()
+        {
+        }
+
+        public TooManyRequestsException(string message)
+            : base(message)
+        {
+        }
+
+        public TooManyRequestsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
